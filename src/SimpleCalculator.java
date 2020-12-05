188871fded1956c0a004bc95efd38930df089cad
Ethan Brown <54382275+Ethan37@users.noreply.github.com>
2020-12-04 22:07:16 -0500

    Added functions

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -11,6 +11,12 @@ public class SimpleCalculator {
     public int div(int a, int b) {
         return a / b;
     }
+    public int multBy10(int a){
+        return a * 10;
+    }
+    public int divBy10(int a){
+        return a / 10;
+    }
     public static final void main(String[] args) {
         System.out.println("Hey there CIS350.");
     }
